6b5d4cbf5fcea5a5d78d9d5cbd0f7b257b92cb18
JuoJuo <348107781@qq.com>
2020-09-11 20:50:49 +0800

    record find and populate one

--- src/model/Guest.js
+++ src/model/Guest.js
@@ -29,16 +29,26 @@ const OrderSchema = new Schema({
 const Guest = connection.model('Guest', GuestSchema);
 const Order = connection.model('Order', OrderSchema);
 
-// const g1 = new Guest({ phone: '321321', email: 'fdsafds@qq.com', password: 'xxxxx'});
-// const o1 = new Order({ orderTime: new Date(), mealTime: new Date('2020/09/15'), wayOfTakingMeals: '自提1', status: '制作中1'});
+const g1 = new Guest({ phone: '321321', email: 'fdsafds@qq.com', password: 'xxxxx'});
+
+const o1 = new Order({ orderTime: new Date(), mealTime: new Date('2020/09/15'), wayOfTakingMeals: '自提1', status: '制作中1'});
+const o2 = new Order({ orderTime: new Date(), mealTime: new Date('2020/09/16'), wayOfTakingMeals: '自提2', status: '制作中2'});
 
 async function go() {
+  await g1.save();
 
-  const d2 = await Guest.findById('5f5b6c921b861f1c0c2a321f').populate('orders');
-  console.log(d2);
+  await o1.save();
+  await o2.save();
 
-  const d3 = await Order.findById('5f5b6c921b861f1c0c2a3220').populate('guest');
-  console.log(d3);
+  await Guest.findOneAndUpdate({ id: g1.id }, { orders: [ o1.id, o2.id ] });
+
+
+
+  // const d2 = await Guest.findById('5f5b6c921b861f1c0c2a321f').populate('orders');
+  // console.log(d2);
+
+  // const d3 = await Order.findById('5f5b6c921b861f1c0c2a3220').populate('guest');
+  // console.log(d3);
 
   // const rs = await Guest.findById('5f5b6c921b861f1c0c2a321f');
   //
